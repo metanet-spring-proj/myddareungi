76c85aa625e05cea404acdd2a3778f08ec4c6c61
SJLIM <yyvaca30@gmail.com>
2026-04-09 14:38:48 +0900

    스웨거 세팅

--- src/main/java/com/metanet/myddareungi/domain/member/controller/MemberViewController.java
+++ src/main/java/com/metanet/myddareungi/domain/member/controller/MemberViewController.java
@@ -3,6 +3,9 @@ package com.metanet.myddareungi.domain.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
 @Controller
 public class MemberViewController {
 
