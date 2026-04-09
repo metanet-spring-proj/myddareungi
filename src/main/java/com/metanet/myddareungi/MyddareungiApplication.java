11acb9e4a9cac172b53237ef7813be6e6c598437
Yeook Yun <dudhr17050@naver.com>
2026-04-09 10:18:45 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/metanet/myddareungi/MyddareungiApplication.java
@@ -0,0 +1,13 @@
+package com.metanet.myddareungi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyddareungiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyddareungiApplication.class, args);
+	}
+
+}
