2ff9ce7af77771f04e4cac81b02ba4d1d5b80a06
dukduk12 <sallyinner59@naver.com>
2026-04-11 15:57:26 +0900

    feat : admin 페이지에 권한이 admin인 사람만 접근 가능하게 동작하게 수정
    - 1) 기존의 코드에서는 SecurityConfig에 어노테이션이 바져있었음

--- src/main/java/com/metanet/myddareungi/config/SecurityConfig.java
+++ src/main/java/com/metanet/myddareungi/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -25,6 +26,7 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Bean
