304a263787f1d7495a000031c02af8d677b9159a
yunyeook <dudhr17050@naver.com>
2026-04-14 00:37:22 +0900

    fix: GlobalExceptionHanlder 불필요한 코드 삭제

--- src/main/java/com/metanet/myddareungi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/metanet/myddareungi/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,7 @@
 package com.metanet.myddareungi.common.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,14 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 401 - 비밀번호 불일치 (에러페이지 말고 상태코드 반환)
-    @ResponseBody
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
-    }
-
-    // 나머지 예외는 에러페이지로
+    // 예외 발생시 에러페이지로
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex, HttpServletResponse response) {
     	// 응답이 이미 커밋된 경우 (SSE, 스트리밍 등) 뷰 렌더링 불가 → null 반환으로 건너뜀
